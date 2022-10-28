1655f3d6552c2aff31e442b30e3aaf04974a27f6
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-10-27 21:17:04 -0300

    primerCambio

--- blackjack.ts
+++ blackjack.ts
@@ -1,3 +1,11 @@
+let readlineSync = require('readline-sync');
+
+
+
+export class Banca{
+    
+}
+
 export class Blackjack {
     private premio: number;
     private numeroCarta: number;
@@ -9,4 +17,53 @@ export class Blackjack {
         this.palo = "diamantes";
         this.numeroCarta = 1;
     }
+    getPremio (): number {
+        return this.premio;
+    }
+    setPremio (pPremio: number): void {
+        this.premio = pPremio;
+    }
+    
+    ingresarApuesta (pSaldo: number): void {
+        this.premio = this.premio + pSaldo;
+    }
+    finalizarJuego(): void {
+        console.log ("El juego ha finalizado");
+    }
+
+    pedirCartaAleatoria(): number {
+        let carta: number;
+        carta = Math.floor(Math.random () * (14 - 1)) + 1;
+        return carta
+    }
+    iniciarJuego (): void {
+        console.log("Bienvenido a BlackJack")
+        let juego: number = 0;
+        juego += this.pedirCartaAleatoria();
+        console.log ("Total: ", juego);
+        let respuesta = readlineSync.question("Desea pedir otra carta? ");
+        while (respuesta === "si" || respuesta === "no") {
+            if (respuesta === "no") {
+                console.log ("Usted se planta con: ", juego);
+                break
+            }
+            else if (respuesta === "si") {
+                juego += this.pedirCartaAleatoria();
+                console.log ("total: ", juego);
+                if (juego > 21) {
+                    console.log ("Superaste los 21, has perdido.")
+                    break
+                } else if (juego === 21){
+                    console.log ("Felicidades BlackJack");
+                    break
+                } 
+            }  
+            respuesta = readlineSync.question("Desea pedir otra carta? ");
+        }        
+    }
 }
+
+let blackjack1: Blackjack = new Blackjack (500000);
+console.log(blackjack1.pedirCartaAleatoria());
+
+blackjack1.iniciarJuego();
