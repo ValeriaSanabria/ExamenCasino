4e80fe48281e74f9c58bbe7b24cf28475d87f1fa
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-11-01 14:47:37 -0300

    correcciones

--- blackjack.js
+++ blackjack.js
@@ -93,6 +93,7 @@ var Blackjack = /** @class */ (function () {
                             console.log("Empate");
                             console.log("Su saldo es:", pUsuario.cobrarEmpate(pApuesta));
                             this.cantEmpate += 1 * 1;
+                            this.totalDineroEntregado += pApuesta * 1;
                         }
                         else if ((this.totalSumaBanca > this.totalSumaUsuario) && (this.totalSumaBanca < 22)) {
                             console.log("La banca gana");
@@ -113,7 +114,7 @@ var Blackjack = /** @class */ (function () {
         }
     };
     Blackjack.prototype.generarEstadisticas = function () {
-        fs.writeFileSync('datosEstadisticosBlackjack.txt', "\n" + "         Datos recolectados" + "\n" + "Cantidad de dinero apostado: " + this.cantDineroApostado + "\n" + "Victorias de la Banca: " + this.cantGanadasBancas + "\n" + "Perdidas de la Banca: " + this.cantPerdidasBanca + "\n" + "Empates: " + this.cantEmpate + "\n" + "Total de dinero entregado: " + this.totalDineroEntregado + "\n");
+        fs.writeFileSync('datosEstadisticosBlackjack.txt', "\n" + "         Datos recolectados" + "\n" + "Victorias de la Banca: " + this.cantGanadasBancas + "\n" + "Perdidas de la Banca: " + this.cantPerdidasBanca + "\n" + "Empates: " + this.cantEmpate + "\n" + "Total de dinero recaudado: $" + this.cantDineroApostado + "\n" + "Total de dinero entregado: $" + this.totalDineroEntregado + "\n");
         console.log(fs.readFileSync('datosEstadisticosBlackjack.txt', 'utf8'));
     };
     return Blackjack;
