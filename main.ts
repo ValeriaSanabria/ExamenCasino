65430ea2ca85f684d4935a653406a2d6a1610ffd
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-11-01 00:17:23 -0300

    Se agrego para que el usuario agregue su apuesta

--- main.ts
+++ main.ts
@@ -3,11 +3,9 @@ import { Usuario } from "./usuario";
 import { Casino } from "./casino"
 
 export let readlineSync = require('readline-sync');
-
 let usuario1: Usuario = new Usuario (123, "Jugador", 10000);
 let blackjack1: Blackjack = new Blackjack ();
 let casino1 : Casino = new Casino ("Las Estrellas", 100000000, blackjack1);
-
 console.log("");
      
 let juegos: Array<string> = ["Blackjack", "Ruleta", "Tragamonedas simple", "Tragamonedas full"];
@@ -17,8 +15,8 @@ i = readlineSync.keyInSelect(juegos, "Elija su juego");
 
 console.log("Bienvenido a: ", juegos[i]);
         
-if (juegos[i] === "Blackjack") {    
-    casino1.iniciarBlack (blackjack1, 100, usuario1);
+if (juegos[i] === "Blackjack") {           
+    casino1.iniciarBlack (blackjack1, usuario1);
 } else if (juegos[i] === "Ruleta") {
     console.log ("Aca va la ruleta");
 } else if (juegos[i] === "Tragamonedas simple") {
